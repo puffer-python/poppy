5a909ccc22bedc9c65c15bd7d06f928b7d49240d
Dung BV <dung.bv@teko.vn>
2023-02-15 13:57:04 +0700

    Rename Package

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
-long_description = open(os.path.join(HERE, 'README.md')).read()
+long_description = open(os.path.join(HERE, 'README.md'), 'r', encoding='utf8').read()
 setup(
-    name='poppy',
+    name='poppy_python',
     version='1.0.0',
     description='A Poppy Python package',
     long_description=long_description,
     author='Dung BV',
     author_email='bvdzung@gmail.com',
-    packages=['poppy'],
+    packages=['poppy_python'],
     install_requires=[
         'pytz', 'slugify'
     ],
